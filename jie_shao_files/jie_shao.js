8a8533a857b63ee0ee22291b49be82194c5dcef8
Wang Fan <wangfan2319@gmail.com>
2015-02-03 14:02:52 +0800

    1.5.2

--- jie_shao_files/jie_shao.js
+++ jie_shao_files/jie_shao.js
@@ -1,7 +1,7 @@
-// Created by iWeb 3.0.4 local-build-20150130
+// Created by iWeb 3.0.4 local-build-20150203
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
-{var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_2:new IWShadow({blurRadius:10,offset:new IWPoint(4.2426,4.2426),color:'#000000',opacity:0.750000}),shadow_1:new IWShadow({blurRadius:10,offset:new IWPoint(4.2426,4.2426),color:'#000000',opacity:0.750000}),shadow_0:new IWShadow({blurRadius:10,offset:new IWPoint(4.2426,4.2426),color:'#000000',opacity:0.750000})});registry.applyEffects();}
+{var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_1:new IWShadow({blurRadius:10,offset:new IWPoint(4.2426,4.2426),color:'#000000',opacity:0.750000}),shadow_2:new IWShadow({blurRadius:10,offset:new IWPoint(4.2426,4.2426),color:'#000000',opacity:0.750000}),shadow_0:new IWShadow({blurRadius:10,offset:new IWPoint(4.2426,4.2426),color:'#000000',opacity:0.750000})});registry.applyEffects();}
 function hostedOnDM()
 {return false;}
 function onPageLoad()
